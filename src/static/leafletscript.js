7db145423e5c1a002a0a70b91d436cdb63757fba
Jason Li <jasonli.0616@outlook.com>
2023-09-04 21:10:09 -0400

    Create function for adding concert marker on map

--- src/static/leafletscript.js
+++ src/static/leafletscript.js
@@ -1,8 +1,36 @@
 // Initialize map
-var map = L.map('map').setView([43.65, -79.38], 12); // Coordinates: 43.65, -79.38 (Toronto); zoom: 12
+var map = L.map('map')
+    .setView([43.65, -79.38], 12); // Coordinates: 43.65, -79.38 (Toronto); zoom: 12
 
 // Add OpenStreetMap tile layer
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+/**
+ * Add a concert marker to the map.
+ * When clicked, it will display details and the link to the setlist.
+ * 
+ * @param {*} lat latitude
+ * @param {*} lng longitude
+ * @param {*} artistName artist name
+ * @param {*} dateRaw date of concert in dd-MM-yyyy as specified in setlist.fm API
+ * @param {*} venue venue name
+ * @param {*} url setlist url
+ */
+function addConcertMarker(lat, lng, artistName, dateRaw, venue, url) {
+
+    // Format date from dd-MM-yyyy to locale
+    let date = new Date(dateRaw.replaceAll("-", "/")).toLocaleDateString();
+
+    // Create marker (pin)
+    let marker = L.marker([lat, lng]).addTo(map);
+
+    // Show details in tooltip on hover
+    marker.bindTooltip(`${artistName}<br>${date}<br>${venue}`)
+
+    // Open page on setlist.fm in new tab on click
+    marker.on("click", () => window.open(url, "_blank"))
+}
